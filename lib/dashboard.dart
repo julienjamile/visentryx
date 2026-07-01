b6f5cf3560620dd50986f5ddd8033a13342c0a8a
IsaacFajardo101 <isaacarnelfajardo@gmail.com>
2026-07-02 01:46:34 +0800

    First Widget of Dashboard accomplished.

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -6,7 +6,26 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopBar(),
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TotalsCardWidget(label: "TOTAL", value: "1,284", bottomlabel: "Total Students", icon: "assets/images/total_dashboard_icon.png"),
+                TotalsCardWidget(label: "NEEDS\nATTENTION", labelColor: Colors.red, value: "24", bottomlabel: "Action Required", icon: "assets/images/attention_dashboard_icon.png")
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TotalsCardWidget(label: "MONITORING", labelColor: Colors.orange, value: "58", bottomlabel: "Watchlist active", icon: "assets/images/monitoring_dashboard_icon.png"),
+                TotalsCardWidget(label: "ON TRACK", labelColor: Colors.green, value: "1,202", bottomlabel: "Stable Status", icon: "assets/images/ontrack_dashboard_icon.png")
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -69,3 +88,58 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget{
     );
   }
 }
+
+class TotalsCardWidget extends StatelessWidget {
+  final String label;
+  final String icon;
+  final String value;
+  final String bottomlabel;
+  final Color labelColor;
+
+  const TotalsCardWidget({
+    required this.label,
+    required this.value,
+    required this.bottomlabel,
+    required this.icon,
+    this.labelColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 180,
+      height: 150,
+      child: Card(
+        color: Colors.white,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(color: Colors.grey.shade200),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(label,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12, color: labelColor, letterSpacing: 0.5)),
+                  Container(
+                    padding: EdgeInsets.all(3),
+                    child: Image.asset(icon, width: 40, height: 40),
+                  ),
+                ],
+              ),
+              Text(value, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28)),
+              Text(bottomlabel, style: TextStyle(color: Colors.grey, fontSize: 13)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
